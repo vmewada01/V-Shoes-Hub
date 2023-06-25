54e0e243d931ac3bc35026ebd1af919501ccaedd
Vishal Mewada <vishalmewada9826@gmail.com>
2023-06-25 08:58:47 +0530

    server_deploy

--- nike_clone_/src/pages/Products.jsx
+++ nike_clone_/src/pages/Products.jsx
@@ -10,8 +10,9 @@ import {
   Grid,
   Heading,
   Input,
+  Select,
 } from "@chakra-ui/react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Product.css";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
@@ -20,15 +21,15 @@ import { getData } from "../Redux/AppReducer/action";
 import ProductDisplayBox from "./ProductDisplayBox";
 
 const Products = () => {
+  const [filter, setFilter] = useState(true);
 
-    const dispatch  = useDispatch()
-    const data= useSelector((store)=> store.AppReducer.products)
-    console.log(data)
+  const dispatch = useDispatch();
+  const data = useSelector((store) => store.AppReducer.products);
+  console.log(data);
 
-  useEffect(()=> {
-      dispatch(getData())
-  },[])
-
+  useEffect(() => {
+    dispatch(getData());
+  }, []);
 
   return (
     <Box width={"95%"} margin={"auto"}>
@@ -37,149 +38,135 @@ const Products = () => {
         <Flex className="flex-top">
           <Box>New & Featured</Box>
           <Flex gap={"0.5rem"}>
-            <Button variant={"ghost"}>
-              <FilterAltIcon /> Filter
+            <Button variant={"ghost"} onClick={() => setFilter(!filter)}>
+              <FilterAltIcon />
+              {filter ? "Hide Filter" : "Show Filter"}
             </Button>
-            <Button variant={"ghost"}>
-              {" "}
-              Sort By <ExpandMoreIcon />
-            </Button>
+
+            <Select border='none' size='md'  fontWeight='semibold' placeholder="Sort By">
+              <option value="option1">Low to High</option>
+              <option value="option2">High to Low</option>
+              <option value="option3">a to z </option>
+              <option value="option3">z to a </option>
+            </Select>
           </Flex>
         </Flex>
       </Box>
 
       {/* sidebar && main content div */}
       <Flex gap="0.5rem" className="media-query">
-        <Box className="sidebar">
-          <Accordion allowToggle>
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box as="span" flex="1" textAlign="left">
-                    Category
+        {filter && (
+          <Box className="sidebar">
+            <Accordion allowToggle>
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box as="span" flex="1" textAlign="left">
+                      Category
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  <Box>
+                    <input type="checkbox" />
+                    <label>Men</label>
                   </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-                <Box>
-                  <input type="checkbox" />
-                  <label>Men</label>
-                </Box>
-                <Box>
-                  <input type="checkbox" />
-                  <label>Women</label>
-                </Box>
-                <Box>
-                  <input type="checkbox" />
-                  <label>Kids</label>
-                </Box>
-                <Box>
-                  <Button >Apply</Button>
-                </Box>
-              </AccordionPanel>
-            </AccordionItem>
+                  <Box>
+                    <input type="checkbox" />
+                    <label>Women</label>
+                  </Box>
+                  <Box>
+                    <input type="checkbox" />
+                    <label>Kids</label>
+                  </Box>
+                  <Box>
+                    <Button>Apply</Button>
+                  </Box>
+                </AccordionPanel>
+              </AccordionItem>
 
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box as="span" flex="1" textAlign="left">
-                    Brand
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box as="span" flex="1" textAlign="left">
+                      Brand
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  <Box>
+                    <input type="checkbox" />
+                    <label>Nike</label>
                   </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-                <Box>
-                  <input type="checkbox" />
-                  <label>Nike</label>
-                </Box>
-                <Box>
-                  <input type="checkbox" />
-                  <label>Jordan</label>
-                </Box>
-                <Box>
-                  <Button >Apply</Button>
-                </Box>
-              </AccordionPanel>
-            </AccordionItem>
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box as="span" flex="1" textAlign="left">
-                    Price
+                  <Box>
+                    <input type="checkbox" />
+                    <label>Jordan</label>
                   </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-                <Box>
-                  <input type="checkbox" />
-                  <label>Low</label>
-                </Box>
-                <Box>
-                  <input type="checkbox" />
-                  <label>High</label>
-                </Box>
-                <Box>
-                  <Button >Apply</Button>
-                </Box>
-              </AccordionPanel>
-            </AccordionItem>
+                  <Box>
+                    <Button>Apply</Button>
+                  </Box>
+                </AccordionPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box as="span" flex="1" textAlign="left">
+                      Price
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  <Box>
+                    <input type="checkbox" />
+                    <label>Low</label>
+                  </Box>
+                  <Box>
+                    <input type="checkbox" />
+                    <label>High</label>
+                  </Box>
+                  <Box>
+                    <Button>Apply</Button>
+                  </Box>
+                </AccordionPanel>
+              </AccordionItem>
 
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box as="span" flex="1" textAlign="left">
-                    Rating
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box as="span" flex="1" textAlign="left">
+                      Rating
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  <Box>
+                    <input type="checkbox" />
+                    <label>★3 & above </label>
                   </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-                <Box>
-                  <input type="checkbox" />
-                  <label>★2 </label>
-                </Box>
-                <Box>
-                  <input type="checkbox" />
-                  <label>★3 </label>
-                </Box>
-                <Box>
-                  <input type="checkbox" />
-                  <label>★4 </label>
-                </Box>
-                <Box>
-                  <Button >Apply</Button>
-                </Box>
-              </AccordionPanel>
-            </AccordionItem>
-          </Accordion>
-        </Box>
-
+                  <Box>
+                    <input type="checkbox" />
+                    <label>★4 & above</label>
+                  </Box>
+                  <Box>
+                    <Button>Apply</Button>
+                  </Box>
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          </Box>
+        )}
         {/* products and all the things are listed here */}
 
         <Grid className="mainContent">
-      
-       {
-        data.length > 0 && data.map((item)=> {
-            return (
-                <ProductDisplayBox item={item}  />
-            )
-        })
-       }
-
-
-
+          {data.length > 0 &&
+            data.map((item) => {
+              return <ProductDisplayBox item={item} />;
+            })}
         </Grid>
-      
-      
-      
-      
-      
-      
-      
-      
       </Flex>
     </Box>
   );
